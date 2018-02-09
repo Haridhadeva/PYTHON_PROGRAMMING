eb698c06f6f8c41baf1165039b15bbb505646df5
Haridhadeva <36121449+Haridhadeva@users.noreply.github.com>
2018-02-09 18:53:31 +0530

    Create largest_among_three.py

---
+++ Begineer/largest_among_three.py
@@ -0,0 +1,10 @@
+while True:
+    a=int(input("enter a value"))
+    b=int(input("enter  b value"))
+    c=int(input("enter c value"))
+    if a>>b>>c:
+        print("a is bigger")
+    elif b>>a>>c:
+        print("b is bigger")
+    else:
+        print("c is bigger")
