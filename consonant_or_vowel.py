6396b108bdb46ae573f908b6962faae60d3affcb
Haridhadeva <36121449+Haridhadeva@users.noreply.github.com>
2018-02-09 17:18:23 +0530

    Update and rename Begineer/to find whether the character is vowel or consonant to consonant_or_vowel.py

--- consonant_or_vowel.py
+++ consonant_or_vowel.py
@@ -1,5 +1,5 @@
 string=raw_input("enter string")
-if string=='a' or string=='e' or string=='i' or string=='o' or string=='u':
+if string=='a' or string=='e' or string=='i' or string=='o' or string=='u' or :
     print("string is vowel".format(string))
 else:
     print("string is consonant".format(string))
