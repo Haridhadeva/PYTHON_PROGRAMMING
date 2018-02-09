bf93362dca4371a6c66480279cd126b9629900e5
Haridhadeva <36121449+Haridhadeva@users.noreply.github.com>
2018-02-09 18:42:56 +0530

    Create alphabet_r_number.py

---
+++ Begineer/alphabet_r_number.py
@@ -0,0 +1,6 @@
+while True:
+    ch = input("Enter any character: ")
+    if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+        print(ch, "is an alphabet")
+    else:
+        print(ch, "is not an alphabet")
