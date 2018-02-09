6084f51aa8e40e05ab5fb7fbb519b6c1863ac46b
Haridhadeva <36121449+Haridhadeva@users.noreply.github.com>
2018-02-09 17:21:41 +0530

    Create vowel_r_consonant.py

---
+++ Begineer/vowel_r_consonant.py
@@ -0,0 +1,5 @@
+string=raw_input("enter string")
+if string=='a' or string=='e' or string=='i' or string=='o' or string=='u' or :
+    print("string is vowel".format(string))
+else:
+print("string is consonant".format(string))
