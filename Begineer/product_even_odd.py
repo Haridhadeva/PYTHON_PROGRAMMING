967a4661af5b201029e30896980b9722ac611cec
Haridhadeva <36121449+Haridhadeva@users.noreply.github.com>
2018-05-28 15:08:58 +0530

    Create product_even_odd.py

---
+++ Begineer/product_even_odd.py
@@ -0,0 +1,8 @@
+a=int(input("Enter a value:"))
+b=int(input("Enter b value:"))
+product=a*b
+print("product of the numbers is %d" %product)
+if product%2==0:
+  print("product is a even number")
+else:
+  print("product is a odd number")
